f7bb47917e549b399bc0010e4bc8a57e5688d565
Rajesh Kumar Verma <rajeshkumarverma@T38B>
2017-07-01 07:40:50 +0530

    Make a navigation menu, header and dashboard. Able to fetch data from json file and show them in table, Applied pagination on data

--- home.jsx
+++ home.jsx
@@ -1,12 +1,114 @@
 import React from 'react';
 class Home extends React.Component {
-   render() {
-      return (
-         <div>
-            <h1>Home...</h1>
-         </div>
-      )
-   }
+    constructor(props) {
+        super(props);
+        this.state = { date: new Date() };
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+    render() {
+        return (
+            <div>
+                <section className="content">
+                    <section style={{
+                        border: "1px solid #3C6EB4",
+                        backgroundColor: '#fff', height: '55px', color: 'black', padding: '17'
+                    }} className="container">
+                        <b className="">Dashboard</b>
+                        <b className="pull-right">{this.state.date.toUTCString()}</b>
+                    </section>
+                </section>
+                <br />
+                <section className="content">
+                    <div style={{
+                        border: "1px solid #3C6EB4", backgroundColor: '#fff',
+                        color: 'black', padding: '18'
+                    }}
+                        className="container">
+
+                        {/*Mange users*/}
+                        <div className="col-md-6">
+                            <div className="panel panel-primary" style={{ color: "#8C8B8B", fontStyle: "italic" }}>
+                                <div className="panel-heading">
+                                    <p className="panel-title">
+                                        <a href="#/lms/manage_users">Manage Users</a>
+                                    </p>
+                                </div>
+                                <div className="panel-body" style={{ backgroundColor: "#F7F8FA" }}>
+                                    <div className="col-md-2">
+                                        <a href="#!/lms/manage_users"><img src="images/lms.png" width="90" alt="image not found" /></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        {/*Manage Library Books*/}
+                        <div className="col-md-6">
+                            <div className="panel panel-primary" style={{ color: "#8C8B8B", fontStyle: "italic" }}>
+                                <div className="panel-heading">
+                                    <p className="panel-title">
+                                        <a href="#!/lms/manage_users">********************</a>
+                                    </p>
+                                </div>
+                                <div className="panel-body" style={{ backgroundColor: "#F7F8FA" }}>
+                                    <div className="col-md-2">
+                                        <a href="#!/lms/manage_users"><img src="images/lms.png" width="90" alt="image not found" /></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        {/*Issue/Return Library Books*/}
+                        <div className="col-md-6">
+                            <div className="panel panel-primary" style={{ color: "#8C8B8B", fontStyle: "italic" }}>
+                                <div className="panel-heading">
+                                    <p className="panel-title">
+                                        <a href="#!/lms/manage_users">********************</a>
+                                    </p>
+                                </div>
+                                <div className="panel-body" style={{ backgroundColor: "#F7F8FA" }}>
+                                    <div className="col-md-2">
+                                        <a href="#!/lms/manage_users"><img src="images/lms.png" width="90" alt="image not found" /></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        {/*Manage Library Transactions*/}
+                        <div className="col-md-6">
+                            <div className="panel panel-primary" style={{ color: "#8C8B8B", fontStyle: "italic" }}>
+                                <div className="panel-heading">
+                                    <p className="panel-title">
+                                        <a href="#!/lms/manage_users">********************</a>
+                                    </p>
+                                </div>
+                                <div className="panel-body" style={{ backgroundColor: "#F7F8FA" }}>
+                                    <div className="col-md-2">
+                                        <a href="#!/lms/manage_users"><img src="images/lms.png" width="90" alt="image not found" /></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </section>
+            </div>
+        )
+    }
 }
 
 export default Home;
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Header from './header.jsx';
 import App from './App.jsx';
 import Home from './home.jsx';
 import Message from './message.jsx';
@@ -8,20 +9,34 @@ import FormValidation from './formValidation.jsx';
 import About from './about.jsx';
 import Contact from './contact.jsx';
 import NotFound from './pageNotFound.jsx';
+
+
+
+import ManageUsers from './manage_users.jsx';
+
+
+
+
 import {Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 
    ReactDOM.render((
    <Router history = {hashHistory}>
-      <Route path = "/" component = {App}>
+      <Route path = "/" component = {Header}>
          <IndexRoute component = {Home} />
          <Route path = "/home" component = {Home} />
+
          <Route path = "/message" component = {Message} />
          <Route path = "/http" component = {Http} />
          <Route path = "/formValidation" component = {FormValidation} />
          <Route path = "/about" component = {About} />
          <Route path = "/contact" component = {Contact} />
+
+         <Route path = "/lms/manage_users" component = {ManageUsers} />
+         
          <Route path='*' component={NotFound} />
+
+
       </Route>
    </Router>
 	
